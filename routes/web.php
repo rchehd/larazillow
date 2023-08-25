6f287eee826d0851f988bf48865505fd408bdbd6
rchehd <romachehdkdk@gmail.com>
2023-08-25 19:40:04 +0300

    Resource controller and route model binding

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\IndexController;
+use App\Http\Controllers\ListingController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,5 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [\App\Http\Controllers\IndexController::class, 'index']);
-Route::get('/hello', [\App\Http\Controllers\IndexController::class, 'show']);
+Route::get('/', [IndexController::class, 'index']);
+Route::get('/hello', [IndexController::class, 'show']);
+
+Route::resource('listing', ListingController::class)
+  ->only('index', 'show');
