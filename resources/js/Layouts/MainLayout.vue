512c531e8661e296d1b64726f813a6620f40981d
Roman <roman.chekhaniuk@smile-ukraine.com>
2023-08-26 23:42:59 +0300

    Add Header and message styles

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,3 +1,27 @@
+<template>
+  <header class="border-b border-gray-200 dark:border-gray-700 dark:bg-gray-800 bg-white w-full">
+    <div class="container mx-auto">
+      <nav class="p-4 flex items-center justify-between">
+        <div class="text-lg font-medium">
+          <Link :href="route('listing.index')">Listing</Link>
+        </div>
+        <div class="text-xl text-indigo-600 dark:text-indigo-300 font-bold text-center">
+          <Link :href="route('listing.index')">Larazillow</Link>
+        </div>
+        <div>
+          <Link :href="route('listing.create')" class="bg-indigo-600 hover:bg-indigo-500 text-white font-medium p-2 rounded-md">+ New listing</Link>
+        </div>
+      </nav>
+    </div>
+  </header>
+  <main class="container mx-auto">
+    <div v-if="flashSuccess" class="mb-4 mt-4 border rounded-md shadow-sm border-green-200 dark:border-green-800 bg-green-50 dark:bg-green-900 p-2">
+      {{ flashSuccess }}
+    </div>
+    <slot />
+  </main>
+</template>
+
 <script setup>
 import {Link, usePage } from '@inertiajs/vue3'
 import {computed} from 'vue'
@@ -7,27 +31,5 @@ const page = usePage()
 const flashSuccess = computed(
   () => page.props.flash.success,
 )
-// import {ref} from 'vue'
-
-// const timer = ref(0)
-// setInterval(() => timer.value++, 1000)
 
 </script>
-
-<template>
-  <Link href="/listing">Listing</Link>&nbsp;
-  <Link href="/listing/create">New listing</Link>
-<!--  <div>The page with time {{ timer }}</div>-->
-  <div v-if="flashSuccess" class="success">
-    {{ flashSuccess }}
-  </div>
-
-  <slot />
-</template>
-
-<style scoped>
-  .success {
-    background-color: green;
-    color: white;
-  }
-</style>
