87a401f83cb303d193a25740ced3aa8ebdbf3e04
Roman <roman.chekhaniuk@smile-ukraine.com>
2023-08-27 17:35:59 +0300

    Add Grid and UI component

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="border-b border-gray-200 dark:border-gray-700 dark:bg-gray-800 bg-white w-full">
+  <header class="border-b border-gray-200 dark:border-gray-700 dark:bg-gray-800 bg-white w-full mb-4">
     <div class="container mx-auto">
       <nav class="p-4 flex items-center justify-between">
         <div class="text-lg font-medium">
@@ -15,7 +15,7 @@
     </div>
   </header>
   <main class="container mx-auto">
-    <div v-if="flashSuccess" class="mb-4 mt-4 border rounded-md shadow-sm border-green-200 dark:border-green-800 bg-green-50 dark:bg-green-900 p-2">
+    <div v-if="flashSuccess" class="mb-4 border rounded-md shadow-sm border-green-200 dark:border-green-800 bg-green-50 dark:bg-green-900 p-2">
       {{ flashSuccess }}
     </div>
     <slot />
