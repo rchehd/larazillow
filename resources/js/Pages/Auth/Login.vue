4033793e86ae9c8dc09edf2ac660ae3b3325aca3
Roman <roman.chekhaniuk@smile-ukraine.com>
2023-09-02 21:01:41 +0300

    Add links ro login/registration forms

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -4,22 +4,27 @@
       <div>
         <label for="email" class="label">E-mail</label>
         <input id="email" v-model="form.email" type="text" class="input" />
-        <div class="input-error" v-if="form.errors.email">{{ form.errors.email }}</div>
+        <div v-if="form.errors.email" class="input-error">{{ form.errors.email }}</div>
       </div>
       <div class="mt-4">
         <label for="password" class="label">Password</label>
         <input id="password" v-model="form.password" type="password" class="input" />
-        <div class="input-error" v-if="form.errors.password">{{ form.errors.password }}</div>
+        <div v-if="form.errors.password" class="input-error">{{ form.errors.password }}</div>
       </div>
       <div class="mt-4">
         <button class="btn-primary w-full" type="submit">Login</button>
+        <div class="mt-2 text-center">
+          <Link :href="route('user-account.create')" class="text-sm text-gray-500">
+            Need an account? Click here
+          </Link>
+        </div>
       </div>
     </div>
   </form>
 </template>
 
 <script setup>
-import {useForm} from '@inertiajs/vue3'
+import {useForm, Link} from '@inertiajs/vue3'
 const form = useForm({
   email: null,
   password: null,
