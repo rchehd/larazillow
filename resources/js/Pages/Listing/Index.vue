c20d6d4fae1f470c7235f50d0a7b6c20c6373245
Roman <roman.chekhaniuk@smile-ukraine.com>
2023-09-03 13:04:18 +0300

    Add filtering form

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,10 +1,11 @@
 <template>
+  <Filters />
   <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-col-4 gap-4">
     <Listing v-for="listing in listings.data" :key="listing.id" :listing="listing" />
   </div>
   <div
     v-if="listings.links"
-    class="w-full flex justify-center mt-8 mb-8"
+    class="w-full flex justify-center mt-4 mb-4"
   >
     <Pagination :links="listings.links" />
   </div>
@@ -14,6 +15,7 @@
 <script setup>
 import Listing from '@/Pages/Listing/Index/Components/Listing.vue'
 import Pagination from '@/Components/UI/Pagination.vue'
+import Filters from '@/Pages/Listing/Index/Components/Filters.vue'
 
 defineProps({
   listings: Object,
