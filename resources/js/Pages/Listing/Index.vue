abae0e732970104d76be8499b1c9dd0f848db707
Roman <roman.chekhaniuk@smile-ukraine.com>
2023-09-03 12:26:07 +0300

    Add pagination

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,15 +1,22 @@
 <template>
   <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-col-4 gap-4">
-    <Listing v-for="listing in listings" :key="listing.id" :listing="listing"/>
+    <Listing v-for="listing in listings.data" :key="listing.id" :listing="listing" />
+  </div>
+  <div
+    v-if="listings.links"
+    class="w-full flex justify-center mt-8 mb-8"
+  >
+    <Pagination :links="listings.links" />
   </div>
 </template>
 
 
 <script setup>
 import Listing from '@/Pages/Listing/Index/Components/Listing.vue'
+import Pagination from '@/Components/UI/Pagination.vue'
 
 defineProps({
-  listings: Array,
+  listings: Object,
 })
 
 </script>
