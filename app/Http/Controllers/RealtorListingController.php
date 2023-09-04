5926723344c063052fc4fbdb6cb5280bc89e7aba
rchehd <romachehdkdk@gmail.com>
2023-09-04 20:18:41 +0300

    Add group routing

---
+++ app/Http/Controllers/RealtorListingController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RealtorListingController extends Controller
+{
+    public function index()
+    {
+        return inertia('Realtor/Index');
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ListingController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserAccountController;
+use App\Http\Controllers\RealtorListingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -34,3 +35,10 @@ Route::delete('logout', [AuthContrtoller::class, 'destroy'])->name('logout');
 
 
 Route::resource('user-account', UserAccountController::class)->only('create', 'store');
+
+Route::prefix('realtor')
+    ->name('realtor.')
+    ->middleware('auth')
+    ->group(function () {
+        Route::resource('listing', RealtorListingController::class);
+    });
