7dbfcacefb5ed8f2ac4a3d7e837aed6ddfcef883
rchehd <romachehdkdk@gmail.com>
2023-09-04 21:00:05 +0300

    Add User listing template

--- app/Http/Controllers/RealtorListingController.php
+++ app/Http/Controllers/RealtorListingController.php
@@ -2,13 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Listing;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class RealtorListingController extends Controller
 {
     public function index()
     {
-        return inertia('Realtor/Index');
+        return inertia('Realtor/Index', [
+            'listings' => Auth::user()->listings,
+        ]);
 
     }
 }
