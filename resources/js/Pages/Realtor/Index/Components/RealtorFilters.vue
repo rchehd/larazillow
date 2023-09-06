25e0ccaf164f8131852930e6fa9a86422f1b3b91
rchehd <romachehdkdk@gmail.com>
2023-09-06 20:35:44 +0300

    Add Debounce function

--- resources/js/Pages/Realtor/Index/Components/RealtorFilters.vue
+++ resources/js/Pages/Realtor/Index/Components/RealtorFilters.vue
@@ -18,15 +18,16 @@
 <script setup>
 import { reactive, watch } from 'vue'
 import { router } from '@inertiajs/vue3'
+import { debounce } from "lodash";
 
 const filterForm = reactive({
   deleted: false,
 })
 // reactive / ref / computed
-watch(filterForm, () => router.get(
+watch(filterForm, debounce(() => router.get(
   route('realtor.listing.index'),
   filterForm,
   {preserveState: true, preserveScroll: true},
-))
+), 1000))
 
 </script>
