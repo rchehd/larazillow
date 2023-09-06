ef61bfe281b8e20c3d6d80a8701da7732571ba3e
rchehd <romachehdkdk@gmail.com>
2023-09-06 20:47:35 +0300

    Add filtering deleted

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -50,6 +50,10 @@ class Listing extends Model
             ->when(
                 $filters['areaTo'] ?? false,
                 fn ($query, $value) => $query->where('area', '<=', $value),
+            )
+            ->when(
+              $filters['deleted'] ?? false,
+              fn ($query, $value) => $query->withTrashed(),
             );
 
     }
