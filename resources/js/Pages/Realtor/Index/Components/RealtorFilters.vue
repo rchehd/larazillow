146325b041cc7b25f91c8d6c8f09a3ed9b3d00e3
rchehd <romachehdkdk@gmail.com>
2023-09-06 20:27:57 +0300

    Add Vue watcher

--- resources/js/Pages/Realtor/Index/Components/RealtorFilters.vue
+++ resources/js/Pages/Realtor/Index/Components/RealtorFilters.vue
@@ -2,8 +2,9 @@
   <form>
     <div class="mb-4 mt-4 flex flex-wrap gap-2">
       <div class="flex flex-nowrap items-center gap-2">
-        <input
+        <input 
           id="deleted"
+          v-model="filterForm.deleted"
           type="checkbox"
           class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500 "
         />
@@ -12,3 +13,20 @@
     </div>
   </form>
 </template>
+
+
+<script setup>
+import { reactive, watch } from 'vue'
+import { router } from '@inertiajs/vue3'
+
+const filterForm = reactive({
+  deleted: false,
+})
+// reactive / ref / computed
+watch(filterForm, () => router.get(
+  route('realtor.listing.index'),
+  filterForm,
+  {preserveState: true, preserveScroll: true},
+))
+
+</script>
