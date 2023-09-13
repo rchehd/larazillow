38d53f8582cb7af0f887154b82d51f149b4bb31f
rchehd <romachehdkdk@gmail.com>
2023-09-13 22:11:55 +0300

    Add "Accepted" label to offer

--- resources/js/Pages/Realtor/Show.vue
+++ resources/js/Pages/Realtor/Show.vue
@@ -8,7 +8,7 @@
         No offers
       </div>
     </Box>
-    <div v-else class="md:col-span-7 items-center">
+    <div v-else class="md:col-span-7 flex flex-col gap-4">
       <Offer 
         v-for="offer in listing.offers"
         :key="offer.id"
@@ -17,13 +17,15 @@
         class="mb-4"
       />
     </div>
-    <Box class="md:col-span-5">
-      <template #header>Basic info</template>
-      <Price :price="listing.price" class="text-2xl font-bold" />
+    <div class="md:col-span-5">
+      <Box >
+        <template #header>Basic info</template>
+        <Price :price="listing.price" class="text-2xl font-bold" />
 
-      <ListingSpace :listing="listing" class="text-lg" />
-      <ListingAddress :listing="listing" class="text-gray-500" />
-    </Box>
+        <ListingSpace :listing="listing" class="text-lg" />
+        <ListingAddress :listing="listing" class="text-gray-500" />
+      </Box>
+    </div>
   </section>
 </template>
 
--- resources/js/Pages/Realtor/Show/Components/Offer.vue
+++ resources/js/Pages/Realtor/Show/Components/Offer.vue
@@ -1,6 +1,14 @@
 <template>
   <Box>
-    <template #header>Offer #{{ offer.id }}</template>
+    <template #header>
+      Offer #{{ offer.id }}
+      <span
+        v-if="offer.accepted_at"
+        class="dark:bg-green-900 dark:text-green-200 bg-green-200 text-green-900 p-1 rounded-md uppercase ml-1"
+      >
+        accepted
+      </span>
+    </template>
 
     <section class="flex items-center justify-between">
       <div>
@@ -19,6 +27,7 @@
       </div>
       <div>
         <Link
+          v-if="notSold"
           :href="route('realtor.offer.accept', { offer: offer.id })"
           class="btn-outline text-xs font-medium" 
           as="button"
@@ -48,5 +57,9 @@ const difference = computed(
 
 const madeOn = computed(
   () => new Date(props.offer.created_at).toDateString(),
+)
+
+const notSold = computed(
+  () => !props.offer.accepted_at && !props.offer.rejected_at,
 )
 </script>
